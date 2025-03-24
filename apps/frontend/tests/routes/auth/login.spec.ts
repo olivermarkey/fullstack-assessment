37d7817bdca421ef55658f76a4350f75bf92d29e
Oliver Markey <oliver.markey@lexinsolutions.com>
2025-03-24 14:55:10 +1100

    Debug playwright sc

--- apps/frontend/tests/routes/auth/login.spec.ts
+++ apps/frontend/tests/routes/auth/login.spec.ts
@@ -5,12 +5,20 @@ import { test, expect } from "@playwright/test";
 test("login with Cognito", async ({ page }) => {
   await page.goto("/login");
 
-  console.log("[TEST] TEST_USER_EMAIL: ", process.env.TEST_USER_EMAIL);
-  
   await page.fill("#email", process.env.TEST_USER_EMAIL!);
   await page.fill("#password", process.env.TEST_USER_PASSWORD!);
+
   await page.click("button[type='submit']");
 
-  await expect(page).toHaveURL("/");
-  await expect(page.getByRole('heading', { name: 'Home' })).toBeVisible();
+  try {
+    await expect(page).toHaveURL("/", { timeout: 8000 });
+  } catch (err) {
+    await page.screenshot({
+      path: "test-artifacts/login-failure.png",
+      fullPage: true,
+    });
+    throw err;
+  }
+  
+  await expect(page.getByRole("heading", { name: "Home" })).toBeVisible();
 });
